6b4b7538bb9803e907f23778f9ad7bc657ddb8c1
sweedalgharshi <sweedalgracius@gmail.com>
2023-03-21 14:23:51 +0530

    added home , banner , row and card styling

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,43 @@
 import React from "react";
-
 import "./Home.scss";
 
+const Card = ({ image }) => {
+  return <img className="card" src={image} alt="cover" />;
+};
+
+const Row = ({
+  title,
+  arr = [
+    {
+      img: "https://images-cdn.ubuy.co.in/639d26c59c29847e8e377f83-the-last-of-us-part-ii-gaming-poster.jpg",
+    },
+  ],
+}) => {
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div>
+        {arr.map((item) => (
+          <Card image={item.img} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="home">
+      <div className="banner"></div>
+
+      <Row title={"Popular on Netflix"} />
+      <Row title={"Movies"} />
+      <Row title={"Tv Shows"} />
+      <Row title={"Recently Viewed"} />
+      <Row title={"My List"} />
+    </section>
+  );
 };
 
 export default Home;
